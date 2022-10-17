9268c10d3d5e8681ca43b92ea8d3f86badedd6b7
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-10-17 07:06:57 -0700

    Update sb6.py

--- sb6.py
+++ sb6.py
@@ -1,25 +1,36 @@
 import datetime
 import time
 
+import ta
 from ccxt import kucoinfutures as kucoin
 from pandas import DataFrame as dataframe
-from ta import momentum, trend
+from ta import trend, volatility
 
 API_KEY = ''
 API_SECRET = ''
 API_PASSWD = ''
-COINS = ['ETH']
+SYMBOL = 'ETC'
 STOPLOSS = 0.02
 TAKEPROFIT = 0.05
 TF = '1m'
 LEVERAGE = 20
-LOTSPERTRADE = 1
+LOTSPERTRADE = 1000
+
 exchange = kucoin({
     'adjustForTimeDifference': True,
     'apiKey': API_KEY,
     'secret': API_SECRET,
     'password': API_PASSWD
 })
+trail = 0
+stop = trail - STOPLOSS
+side = 'none'
+contracts = 0
+pnl = 0
+coin = str(f'{SYMBOL}/USDT:USDT')
+positions = exchange.fetch_positions()
+equity = exchange.fetch_balance()['info']['data']['accountEquity']
+
 print('\n'*100, 'TRADE AT YOUR OWN RISK. CRYPTOCURRENCY FUTURES TRADES ARE NOT FDIC INSURED. RESULTS ARE NOT GUARANTEED. POSITIONS MAY LOSE VALUE SUDDENLY AND WITHOUT WARNING. POSITINOS ARE SUBJECT TO LIQUIDATION. THERE ARE RISKS ASSOCIATED WITH ALL FORMS OF TRADING. IF YOU DON\'T UNDERSTAND THAT, THEN YOU SHOULD NOT BE TRADING IN THE FIRST PLACE. THIS SOFTWARE IS DEVELOPED FOR MY OWN USE, AND IS NOT TO BE INTERPRETED AS FINANCIAL ADVICE.')
 time.sleep(1)
 print('\n'*100, '...AND MOST OF ALL HAVE FUN!!\n')
@@ -32,7 +43,7 @@ def getData(coin, TF):
     data = exchange.fetch_ohlcv(coin, TF, limit=200)
     df = {}
     for i, col in enumerate(['date', 'open', 'high', 'low', 'close',
-                            'volume']):
+                                                     'volume']):
         df[col] = []
         for row in data:
             if col == 'date':
@@ -54,7 +65,7 @@ def order(type):
                       'closeOrder': True, 'timeInForce': 'GTC'}
         elif side != 'short':
             amount = LOTSPERTRADE
-            params = {'leverage': LEVERAGE, 'timeInForce': 'GTC'}
+            params = {'leverage': LEVERAGE, 'timeInForce': 'IOC'}
     elif type == 'sell':
         print(f'SELL {coin}')
         price = exchange.fetch_order_book(coin)['bids'][0][0]
@@ -64,73 +75,83 @@ def order(type):
                       'closeOrder': True, 'timeInForce': 'GTC'}
         elif side != 'long':
             amount = LOTSPERTRADE
-            params = {'leverage': LEVERAGE, 'timeInForce': 'GTC'}
+            params = {'leverage': LEVERAGE, 'timeInForce': 'IOC'}
     return exchange.create_limit_order(coin, type, amount, price, params=params)
 
 
 def ema(w):
-    close = (getData(coin, TF)['close'])
-    return trend.ema_indicator(close, w).iloc[-1]
+    return trend.ema_indicator((getData(coin, TF)['close']), w).iloc[-1]
 
 
-def macd(slow, fast):
-    close = (getData(coin, TF)['close'])
-    return trend.macd(close, slow, fast).iloc[-1]
+def macd(fast, slow, signal):
+    return 1 if (ema(fast) - ema(slow)) < ema(signal) else -1 if (ema(fast) - ema(slow)) > ema(signal) else 0
+
+
+class bb:
+    def h(window, deviations):
+        return volatility.bollinger_hband(getData(coin, TF)['close'], window, deviations).iloc[-1]
+
+    def l(window, deviations):
+        return volatility.bollinger_lband(getData(coin, TF)['close'], window, deviations).iloc[-1]
 
 
 def bot():
-    print(f'{TF} {coin} {side} {contracts} {round((100*pnl),2)}% TOTAL: {equity}')
-
-    if macd(13, 21) < ema(8) and Open > Close:
-        order('sell')
-    elif macd(13, 21) > ema(8) and Open < Close:
+    if Open < bb.l(20, 1) and Close > Open:
         order('buy')
-    elif ema(5) > ema(8) > ema(13) and Open < Close:
-        order('buy')
-    elif ema(5) < ema(8) < ema(13) and Open > Close:
+    elif Open > bb.h(20, 1) and Close < Open:
         order('sell')
-    elif side == 'long':
-        if (ema(2) < ema(3)) or (
-                ema(3) < ema(5)) or (
-                macd(8, 13) < ema(5)):
-            order('sell')
-    elif side == 'short':
-        if (ema(5) > ema(8)) or (
-                ema(8) > ema(13)) or (
-                macd(8, 13) > ema(5)):
-            order('buy')
+    elif macd(8, 13, 5) > 0 and Close > Open:
+        order('buy')
+    elif macd(8, 13, 5) < 0 and Close < Open:
+        order('sell')
 
 
-trail = 0
 while True:
     try:
-        positions = exchange.fetch_positions()
-        equity = exchange.fetch_balance()['info']['data']['accountEquity']
-        for symbol in COINS:
-            coin = str(f'{symbol.upper()}/USDT:USDT')
-            Open = (getData(coin, TF)['close'].iloc[-2] +
-                    getData(coin, TF)['open'].iloc[-2])/2
-            Close = (getData(coin, TF)['open'].iloc[-1]+getData(coin, TF)['high'].iloc[-1]+getData(
-                coin, TF)['low'].iloc[-1]+getData(coin, TF)['close'].iloc[-1])/4
-            pnl = float(0.0)
-            contracts = int(0)
-            side = str('none')
-            for i in positions[0:]:
+        Open = (getData(coin, TF)['close'].iloc[-2] +
+                getData(coin, TF)['open'].iloc[-2])/2
+        Close = (getData(coin, TF)['open'].iloc[-1] +
+                 getData(coin, TF)['high'].iloc[-1] +
+                 getData(coin, TF)['low'].iloc[-1] +
+                 getData(coin, TF)['close'].iloc[-1])/4
+        High = getData(coin, TF)['high'].iloc[-1]
+        try:
+            for ii,i in enumerate(positions):
                 if coin == i['symbol']:
                     pnl = i['percentage']
+                    side = i['side']
+                    contracts = i['contracts']
                     if pnl > trail:
                         trail = pnl
+                        stop = trail - abs(STOPLOSS)
                         print(
-                            f'TRAIL: {round(trail*100,2)}% -- STOP: {round(trail+STOPLOSS*100, 2)}%')
-                    if pnl <= trail+STOPLOSS or pnl > TAKEPROFIT:
+                            f'TRAIL: {round(trail*100,2)}% -- STOP: {round(stop*100, 2)}%')
+                    if pnl <= stop:
                         if side == 'long':
                             order('sell')
                         if side == 'short':
                             order('buy')
-                    side = i['side']
-                    contracts = i['contracts']
+                    print(
+                        f'{TF} {coin} {side} {contracts} {round((100*pnl),2)}% TOTAL: {equity}')
                     bot()
+        except Exception:
+            pnl = 0.0
+            contracts = 0
+            side = 'none'
+            trail = 0
+            if pnl > trail:
+                trail = pnl
+            stop = trail - abs(STOPLOSS)
+            print(
+                f'TRAIL: {round(trail*100,2)}% -- STOP: {round(stop*100, 2)}%')
+            if pnl <= stop:
+                if side == 'long':
+                    order('sell')
+                if side == 'short':
+                    order('buy')
+            print(
+                f'{TF} {coin} {side} {contracts} {round((100*pnl),2)}% TOTAL: {equity}')
             bot()
     except Exception as e:
-        exchange.cancel_all_orders()
         print(e)
+        time.sleep(10)
